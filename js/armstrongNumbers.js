61574f253188acd3073953eafc486014c3b7ad93
Justin <jjpeterson90@hotmail.com>
2022-05-27 10:15:40 -0500

    complete

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,12 +1,13 @@
 // How can you make this more scalable and reusable later?
 
 exports.findArmstrongNumbers = function(input) {
+//let findArmstrongNumbers = function(input) {
 
     let result = [];
     let numString;
     let answer;
 
-    for (let num = 1; num <= input; num++) {
+    for (let num = 0; num < input; num++) {
         answer = 0;
         numString = num.toString().split('');
         expo = numString.length;
@@ -22,5 +23,7 @@ exports.findArmstrongNumbers = function(input) {
     return result
 
 };
+
+//findArmStrongNumbers()
 
 //console.log(findArmstrongNumbers(235))
